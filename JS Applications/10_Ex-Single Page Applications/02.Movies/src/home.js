ce01a69815c0dbf4a7d4b734da2e889c95e22d26
al-badarin <jamalalbadarin@gmail.com>
2023-10-31 22:03:50 +0200

    implemented functionality - all added movies are visible on home page

--- JS Applications/10_Ex-Single Page Applications/02.Movies/src/home.js
+++ JS Applications/10_Ex-Single Page Applications/02.Movies/src/home.js
@@ -2,7 +2,44 @@ import { showView } from "./util.js";
 
 const section = document.getElementById('home-page');
 
-export function homePage(){
+const catalog = section.querySelector(
+    '#movie .card-deck.d-flex.justify-content-center'
+);
+
+export function homePage() {
 
     showView(section);
+
+    displayMovies();
+}
+
+async function displayMovies() {
+    const movies = await getMovies();
+
+    catalog.replaceChildren(...movies.map(createMoviePreview));
+}
+
+function createMoviePreview(movie) {
+    const liElem = document.createElement('li');
+    liElem.className = 'card mb-4';
+    liElem.innerHTML = `
+    <img class="card-img-top" src="${movie.img}"
+        alt="Card image cap" width="400">
+    <div class="card-body">
+        <h4 class="card-title">${movie.title}</h4>
+        <a href="/details/${movie._id}">
+            <button data-id="${movie._id} type="button" class="btn btn-info">Details</button>
+        </a>
+    </div>
+    <div class="card-footer">
+    </div>`;
+
+    return liElem;
+}
+
+async function getMovies() {
+    const res = await fetch('http://localhost:3030/data/movies');
+    const movies = await res.json();
+
+    return movies;
 }
