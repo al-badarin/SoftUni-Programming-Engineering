b5a5866da1a52dca9fccbc35f7798956be2df0f8
al-badarin <jamalalbadarin@gmail.com>
2023-11-05 00:12:07 +0200

    added showPost functionality

--- JS Applications/10_Ex-Single Page Applications/01.Forum/src/details.js
+++ JS Applications/10_Ex-Single Page Applications/01.Forum/src/details.js
@@ -1,4 +1,16 @@
 const section = document.getElementById('detailsView');
+const postElement = {
+    title: document.getElementById('details-title'),
+    username: document.getElementById('details-username'),
+    time: document.getElementById('details-time'),
+    content: document.getElementById('details-content'),
+};
+
+const commentsList = document.getElementById('user-comment');
+
+const form = section.querySelector('form');
+form.addEventListener('submit', onsubmit);
+
 section.remove();
 
 
@@ -10,11 +22,35 @@ export function showDetails(ev) {
     }
     if (target.tagName == 'A') {
         ev.preventDefault();
-        showPost();
+
+        const postId = target.id;
+        showPost(postId);
     }
 }
 
-function showPost(postId) {
-    document.getElementById('main').replaceChildren(section);
+async function showPost(postId) {
+    document.getElementById('main').replaceChildren('Loading...');
 
+    const [res, commentsRes] = await Promise.all([
+        fetch('http://localhost:3030/jsonstore/collections/myboard/posts/' + postId),
+        fetch('http://localhost:3030/jsonstore/collections/myboard/comments')
+    ]);
+
+    const [post, comments] = await Promise.all([
+        res.json(),
+        commentsRes.json()
+    ]);
+
+    commentsList.replaceChildren(...Object
+        .values(comments)
+        .filter(c => c.postId == postId)
+        .map(createCommentElement));
+
+    form.id = postId;
+    postElement.title.textContent = post.title; 
+    postElement.username.textContent = post.username; 
+    postElement.time.textContent = post.dateCreated; 
+    postElement.content.textContent = post.content; 
+
+    document.getElementById('main').replaceChildren(section);
 }
