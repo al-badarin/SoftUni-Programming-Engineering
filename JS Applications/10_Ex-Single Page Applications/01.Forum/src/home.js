5e062b8842b8c24f535cc883488aaf17096f2ef6
al-badarin <jamalalbadarin@gmail.com>
2023-11-04 19:17:23 +0200

    added createPostPreview func + edited showHome func - task 01.Forum

--- JS Applications/10_Ex-Single Page Applications/01.Forum/src/home.js
+++ JS Applications/10_Ex-Single Page Applications/01.Forum/src/home.js
@@ -9,14 +9,45 @@ form.addEventListener('submit', onSubmit);
 
 section.querySelector('[name="cancel"]').addEventListener('click', clearForm);
 
+const container = section.querySelector('.topic-container');
+
 section.remove();
 
 
 export async function showHome(ev) {
     ev?.preventDefault();
 
-    document.getElementById('main').replaceChildren(section);
+    //Loading
+    document.getElementById('main').replaceChildren('Loading...');
+    const res = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts')
+    const posts = await res.json();
 
+    container.replaceChildren(...Object.values(posts).map(createPostPreview));
+
+    document.getElementById('main').replaceChildren(section);
+}
+
+function createPostPreview(post){
+    const element = document.createElement('div');
+    element.className = 'topic-name-wrapper';
+    element.innerHTML = `
+    <div class="topic-name">
+        <a href="/details" class="normal" id="${post._id}">
+            <h2>${post.title}</h2>
+        </a>
+        <div class="columns">
+            <div>
+                <p>Date: <time>${post.dateCreated}</time></p>
+                <div class="nick-name">
+                    <p>Username: <span>${post.username}</span></p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>`;
+
+    return element;
 }
 
 async function onSubmit(ev) {
@@ -36,7 +67,8 @@ async function onSubmit(ev) {
             body: JSON.stringify({
                 title,
                 username,
-                content
+                content,
+                dateCreated: new Date()
             })
         });
 
