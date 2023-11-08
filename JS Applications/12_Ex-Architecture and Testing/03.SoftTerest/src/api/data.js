3d0e195598128cad5b729350c2945a6d7a5b76bc
al-badarin <jamalalbadarin@gmail.com>
2023-11-08 15:43:02 +0200

    added delete and details functionality

--- JS Applications/12_Ex-Architecture and Testing/03.SoftTerest/src/api/data.js
+++ JS Applications/12_Ex-Architecture and Testing/03.SoftTerest/src/api/data.js
@@ -3,18 +3,22 @@ import * as api from './api.js';
 
 const endpoints = {
     'ideas': '/data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc',
-    'ideaById': 'data/ideas/',
+    'ideaById': '/data/ideas/',
     'create': '/data/ideas'
 };
 
-export async function getAllIdeas(){
+export async function getAllIdeas() {
     return api.get(endpoints.ideas);
 }
 
-export async function getById(id){
+export async function getById(id) {
     return api.get(endpoints.ideaById + id);
 }
 
-export async function createIdea(ideaData){
+export async function createIdea(ideaData) {
     return api.post(endpoints.create, ideaData)
+}
+
+export async function deleteById(id) {
+    return api.delete(endpoints.ideaById + id);
 }
