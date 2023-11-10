e3dfc209c059831fe479b21d8efe6a8d34da4e88
al-badarin <jamalalbadarin@gmail.com>
2023-11-10 22:17:26 +0200

    implemented search functionality; task 05. solved - 100/100

--- JS Applications/14_Ex-Client Side Rendering/task-solutions/05.Table-Search/src/app.js
+++ JS Applications/14_Ex-Client Side Rendering/task-solutions/05.Table-Search/src/app.js
@@ -1,6 +1,7 @@
 import { render } from '/node_modules/lit-html/lit-html.js';
 import { getAllStudents } from "./api.js";
 import { studentsTemplate } from "./studentsTemplate.js";
+import { onSearch } from './search.js';
 
 
 const tableBody = document.querySelector(".container tbody");
@@ -9,3 +10,5 @@ const studentsData = await getAllStudents();
 
 render(studentsTemplate(Object.values(studentsData)), tableBody);
 
+const searchButton = document.getElementById("searchBtn");
+searchButton.addEventListener("click", onSearch);
