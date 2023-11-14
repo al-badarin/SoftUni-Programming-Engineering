a2ce6d2a64dfd722b057c36015db849f978455ea
al-badarin <jamalalbadarin@gmail.com>
2023-11-14 18:07:47 +0200

    implemented updateNav functionality

---
+++ JS Applications/16_Ex-Routing/task-solutions/01.Furniture/src/app.js
@@ -0,0 +1,4 @@
+import page from "/node_modules/page/page.mjs";
+import { updateNav } from "./utils.js";
+
+updateNav();
---
+++ JS Applications/16_Ex-Routing/task-solutions/01.Furniture/src/views/catalogView.js
@@ -0,0 +1,7 @@
+import { render, html } from "/node_modules/lit-html/lit-html.js";
+
+const catalogTemplate = (data) => html``;
+
+export function catalogView(){
+    
+}
