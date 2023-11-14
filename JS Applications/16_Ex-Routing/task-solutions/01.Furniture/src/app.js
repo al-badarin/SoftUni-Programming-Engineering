c0dc651aca4b4ab0fa1d4afed3c9aa34a3a44d38
al-badarin <jamalalbadarin@gmail.com>
2023-11-14 23:22:03 +0200

    implemented catalogView.js

--- JS Applications/16_Ex-Routing/task-solutions/01.Furniture/src/app.js
+++ JS Applications/16_Ex-Routing/task-solutions/01.Furniture/src/app.js
@@ -1,4 +1,8 @@
-import page from "/node_modules/page/page.mjs";
+import page from "../node_modules/page/page.mjs";
 import { updateNav } from "./utils.js";
+import { catalogView } from "./views/catalogView.js";
 
 updateNav();
+
+page("/", catalogView);
+page.start();
