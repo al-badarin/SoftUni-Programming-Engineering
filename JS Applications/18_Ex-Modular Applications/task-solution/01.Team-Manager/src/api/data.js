b2b41dff68ded28071f57c6d52fe8cb623ed6a52
al-badarin <jamalalbadarin@gmail.com>
2023-11-18 23:51:23 +0200

    started implementing details.js

--- JS Applications/18_Ex-Modular Applications/task-solution/01.Team-Manager/src/api/data.js
+++ JS Applications/18_Ex-Modular Applications/task-solution/01.Team-Manager/src/api/data.js
@@ -3,6 +3,12 @@ import * as api from "./api.js";
 const endpoints = {
     allTeams: "/data/teams",
     allTeamMembers: "/data/members",
+    teamById: "/data/teams/",
+    teamMembersByTeamId: (id) =>
+        `/data/members?where=${encodeURIComponent(
+            `teamId="${id}"`
+        )}&load=user%3D_ownerId%3Ausers`,
+    deleteTeamMemberById: "/data/members/",
 }
 
 export async function getAllTeams() {
@@ -11,4 +17,17 @@ export async function getAllTeams() {
 
 export async function getAllTeamMembers() {
     return api.get(endpoints.allTeamMembers);
+}
+
+export async function getTeamById(id) {
+    return api.get(endpoints.teamById + id);
+}
+
+export async function getTeamMembersByTeamId(id) {
+    return api.get(endpoints.teamMembersByTeamId(id));
+}
+
+
+export async function deleteTeamMemberById(id) {
+    return api.del(endpoints.deleteTeamMemberById + id);
 }
