49a1d10dfb4a56fa02d18c24a566f41360764faf
al-badarin <jamalalbadarin@gmail.com>
2023-11-19 16:22:50 +0200

    added Exam Preparation folder and started the solution of Music-App
    Music-App:
    - app.js
    - middlewares folder: renderMiddlewares.js
    - started navigationView.js and loginView.js

---
+++ JS Applications/EXAM-PREPARATION/Music-App/src/views/loginView.js
@@ -0,0 +1,27 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const logintemplate = () => html`
+    <section id="loginPage">
+        <form>
+            <fieldset>
+                <legend>Login</legend>
+
+                <label for="email" class="vhide">Email</label>
+                <input id="email" class="email" name="email" type="text" placeholder="Email">
+
+                <label for="password" class="vhide">Password</label>
+                <input id="password" class="password" name="password" type="password" placeholder="Password">
+
+                <button type="submit" class="login">Login</button>
+
+                <p class="field">
+                    <span>If you don't have profile click <a href="#">here</a></span>
+                </p>
+            </fieldset>
+        </form>
+    </section>
+`;
+
+export const loginView = (ctx) => {
+    console.log('...loginView...');
+}
