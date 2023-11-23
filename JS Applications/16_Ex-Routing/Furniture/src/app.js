e27e1d55011fceac515bb185e452cddae409b584
al-badarin <jamalalbadarin@gmail.com>
2023-11-24 00:01:46 +0200

    implemented logout func

--- JS Applications/16_Ex-Routing/Furniture/src/app.js
+++ JS Applications/16_Ex-Routing/Furniture/src/app.js
@@ -2,12 +2,14 @@ import page from '../node_modules/page/page.mjs';
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderContentMiddleware } from './middlewares/renderMiddleware.js';
 import { loginView } from './views.js/loginView.js';
+import { logoutView } from './views.js/logoutView.js';
 import { registerView } from './views.js/registerView.js';
 
 page(authMiddleware);
 page(renderContentMiddleware);
 
-page('/login', loginView);
 page('/register', registerView);
+page('/login', loginView);
+page('/logout', logoutView);
 
 page.start();
