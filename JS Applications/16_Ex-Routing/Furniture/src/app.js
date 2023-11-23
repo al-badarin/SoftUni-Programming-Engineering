4fcae5e9119576534f03ce13c5ee647821d65b9c
al-badarin <jamalalbadarin@gmail.com>
2023-11-24 00:43:52 +0200

    start implementing createView.js

--- JS Applications/16_Ex-Routing/Furniture/src/app.js
+++ JS Applications/16_Ex-Routing/Furniture/src/app.js
@@ -1,6 +1,7 @@
 import page from '../node_modules/page/page.mjs';
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderContentMiddleware } from './middlewares/renderMiddleware.js';
+import { createView } from './views.js/createView.js';
 import { loginView } from './views.js/loginView.js';
 import { logoutView } from './views.js/logoutView.js';
 import { registerView } from './views.js/registerView.js';
@@ -11,5 +12,6 @@ page(renderContentMiddleware);
 page('/register', registerView);
 page('/login', loginView);
 page('/logout', logoutView);
+page('/create', createView);
 
 page.start();
--- JS Applications/16_Ex-Routing/Furniture/src/services/furnitureService.js
+++ JS Applications/16_Ex-Routing/Furniture/src/services/furnitureService.js
@@ -1,19 +1,19 @@
 import * as request from './requester.js';
 
-const baseUrl = 'http://localhost:3030/data/furnitures';
+const baseUrl = 'http://localhost:3030/data/catalog';
 
-export const getAll = () => request.get(`${baseUrl}?sortBy=_createdOn%20desc&distinct=name`);
-
-export const getOne = (furnitureId) => request.get(`${baseUrl}/${furnitureId}`);
+export const getAll = () => request.get(baseUrl);
 
 export const create = (furnitureData) => request.post(baseUrl, furnitureData);
 
-export const edit = (furnitureId, furnitureData) => request.put(`${baseUrl}/${furnitureId}`, furnitureData);
+// export const getOne = (furnitureId) => request.get(`${baseUrl}/${furnitureId}`);
 
-export const remove = (furnitureId) => request.del(`${baseUrl}/${furnitureId}`);
+// export const edit = (furnitureId, furnitureData) => request.put(`${baseUrl}/${furnitureId}`, furnitureData);
 
-export const search = (searchText) => {
-    const query = encodeURIComponent(`name LIKE "${searchText}"`);
+// export const remove = (furnitureId) => request.del(`${baseUrl}/${furnitureId}`);
 
-    return request.get(`${baseUrl}?where=${query}`);
-};
+// export const search = (searchText) => {
+//     const query = encodeURIComponent(`name LIKE "${searchText}"`);
+
+//     return request.get(`${baseUrl}?where=${query}`);
+// };
