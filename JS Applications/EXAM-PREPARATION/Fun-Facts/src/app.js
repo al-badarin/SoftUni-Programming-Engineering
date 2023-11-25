7675bb11431fcd8d86d7fb1c7c819635528dc320
al-badarin <jamalalbadarin@gmail.com>
2023-11-25 13:07:49 +0200

    added register functionality

--- JS Applications/EXAM-PREPARATION/Fun-Facts/src/app.js
+++ JS Applications/EXAM-PREPARATION/Fun-Facts/src/app.js
@@ -2,11 +2,13 @@ import page from "../node_modules/page/page.mjs";
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderContentMiddleware, renderNavigationMiddleware } from "./middlewares/renderMiddleware.js";
 import { loginView } from "./views/login.js";
+import { registerView } from "./views/registerView.js";
 
 page(authMiddleware);
 page(renderContentMiddleware);
 page(renderNavigationMiddleware);
 
 page('/login', loginView);
+page('/register', registerView);
 
 page.start();
--- JS Applications/EXAM-PREPARATION/Fun-Facts/src/views/login.js
+++ JS Applications/EXAM-PREPARATION/Fun-Facts/src/views/login.js
@@ -2,7 +2,6 @@ import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import * as userService from '../services/userService.js';
 
 const loginTemplate = (submitHandler) => html`
-    <!-- Login Page (Only for Guest users) -->
     <section id="login">
         <div class="form">
             <h2>Login</h2>
@@ -11,7 +10,7 @@ const loginTemplate = (submitHandler) => html`
             <input type="password" name="password" id="password" placeholder="password" />
             <button type="submit">login</button>
             <p class="message">
-                Not registered? <a href="#">Create an account</a>
+                Not registered? <a href="/register">Create an account</a>
             </p>
             </form>
         </div>
