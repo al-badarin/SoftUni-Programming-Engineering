138de7440fe5d1d8000dc015ff1e55611d655f15
al-badarin <jamalalbadarin@gmail.com>
2023-11-25 13:01:12 +0200

    added login functionality

--- JS Applications/EXAM-PREPARATION/Fun-Facts/src/app.js
+++ JS Applications/EXAM-PREPARATION/Fun-Facts/src/app.js
@@ -1,10 +1,12 @@
 import page from "../node_modules/page/page.mjs";
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderContentMiddleware, renderNavigationMiddleware } from "./middlewares/renderMiddleware.js";
+import { loginView } from "./views/login.js";
 
 page(authMiddleware);
 page(renderContentMiddleware);
 page(renderNavigationMiddleware);
 
+page('/login', loginView);
 
 page.start();
