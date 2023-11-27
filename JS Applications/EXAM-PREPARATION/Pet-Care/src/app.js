6050e684a0d6c04ef89eac58dbfadc4b1c4fb7f8
al-badarin <jamalalbadarin@gmail.com>
2023-11-27 17:50:22 +0200

    implemented dynamic navigation bar

--- JS Applications/EXAM-PREPARATION/Pet-Care/src/app.js
+++ JS Applications/EXAM-PREPARATION/Pet-Care/src/app.js
@@ -1,4 +1,10 @@
 import page from "../node_modules/page/page.mjs";
+import { renderContentMiddleware, renderNavigationMiddleware } from "./middlewares/renderMiddleware.js";
+import {authMiddleware} from "./middlewares/authMiddleware.js";
+
+page(authMiddleware);
+page(renderContentMiddleware);
+page(renderNavigationMiddleware);
 
 
 page.start();
