3361aba6248fdfbc51a62b29d06b522545b2963e
al-badarin <jamalalbadarin@gmail.com>
2023-11-27 18:02:00 +0200

    added homeView.js

--- JS Applications/EXAM-PREPARATION/Pet-Care/src/app.js
+++ JS Applications/EXAM-PREPARATION/Pet-Care/src/app.js
@@ -2,11 +2,13 @@ import page from "../node_modules/page/page.mjs";
 import { renderContentMiddleware, renderNavigationMiddleware } from "./middlewares/renderMiddleware.js";
 import {authMiddleware} from "./middlewares/authMiddleware.js";
 import { loginView } from "./views/loginView.js";
+import { homeView } from "./views/homeView.js";
 
 page(authMiddleware);
 page(renderContentMiddleware);
 page(renderNavigationMiddleware);
 
+page('/', homeView);
 page('/login', loginView);
 
 page.start();
