1552bdefe4e72f2d8ee4a3daf58fd1569db8b1cf
al-badarin <jamalalbadarin@gmail.com>
2023-11-28 16:08:32 +0200

    added login func

--- JS Applications/EXAM-PREPARATION/Samurider/src/app.js
+++ JS Applications/EXAM-PREPARATION/Samurider/src/app.js
@@ -2,13 +2,14 @@ import page from "../node_modules/page/page.mjs";
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderContentMiddleware, renderNavigationMiddleware } from './middlewares/renderMiddleware.js';
 import { homeView } from "./views/homeView.js";
+import { loginView } from "./views/loginView.js";
 
 page(authMiddleware);
 page(renderContentMiddleware);
 page(renderNavigationMiddleware);
 
 page('/', homeView);
-// page('/login', loginView);
+page('/login', loginView);
 // page('/register', registerView);
 // page('/logout', logoutView);
 
