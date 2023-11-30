ee328cb5c260ed3d81bfc9d1299621a4c50c12ee
al-badarin <jamalalbadarin@gmail.com>
2023-11-30 23:39:02 +0200

    added login func

--- JS Applications/EXAM-PREPARATION/Music Library/src/views/loginView.js
+++ JS Applications/EXAM-PREPARATION/Music Library/src/views/loginView.js
@@ -1,10 +1,20 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 import * as userService from '../services/userService.js';
 
-//@submit=${submitHandler}
-//TODO: ADD THE TEMPLATE
 const loginTemplate = (submitHandler) => html`
-
+    <section id="login">
+    <div class="form">
+        <h2>Login</h2>
+        <form class="login-form" @submit=${submitHandler}>
+        <input type="text" name="email" id="email" placeholder="email" />
+        <input type="password" name="password" id="password" placeholder="password" />
+        <button type="submit">login</button>
+        <p class="message">
+            Not registered? <a href="#">Create an account</a>
+        </p>
+        </form>
+    </div>
+    </section>
 `;
 
 export const loginView = (ctx) => {
@@ -13,9 +23,15 @@ export const loginView = (ctx) => {
 
         const { email, password } = Object.fromEntries(new FormData(e.currentTarget));
 
+        if (email == '' ||
+            password == '') {
+            alert('All fields required!')
+            return;
+        }
+
         userService.login(email, password)
             .then(() => {
-                ctx.page.redirect('/');
+                ctx.page.redirect('/catalog');
             })
             .catch(err => {
                 alert(err);
