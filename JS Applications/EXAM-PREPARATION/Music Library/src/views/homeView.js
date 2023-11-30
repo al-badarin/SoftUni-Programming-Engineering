7970bc3f0cc481b6ca33852c566b67bc15e46516
al-badarin <jamalalbadarin@gmail.com>
2023-11-30 23:36:11 +0200

    added homeView.js

--- JS Applications/EXAM-PREPARATION/Music Library/src/views/homeView.js
+++ JS Applications/EXAM-PREPARATION/Music Library/src/views/homeView.js
@@ -1,8 +1,12 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-//TODO: ADD THE TEMPLATE
 const homeTemplate = () => html`
-    
+    <section id="home">
+    <img src="./images/landing.png" alt="home" />
+
+    <h2 id="landing-text"><span>Add your favourite albums</span> <strong>||</strong> <span>Discover new ones right
+        here!</span></h2>
+    </section>
 `;
 
 export const homeView = (ctx) => {
