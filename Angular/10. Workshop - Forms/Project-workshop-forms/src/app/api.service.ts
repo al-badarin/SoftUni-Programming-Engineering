2fdbaec222df0e378a997db78c9cbd4b6535687c
al-badarin <jamalalbadarin@gmail.com>
2024-03-08 00:41:39 +0200

    added '10. Workshop - Forms'

---
+++ Angular/10. Workshop - Forms/Project-workshop-forms/src/app/api.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/theme';
+import { Post } from './types/post';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getTheme(id: string) {
+    const { appUrl } = environment;
+
+    return this.http.get<Theme>(`${appUrl}/themes/${id}`);
+  }
+
+  getThemes() {
+    const { appUrl } = environment;
+
+    return this.http.get<Theme[]>(`${appUrl}/themes`);
+  }
+
+  getPosts(limit?: number) {
+    const { appUrl } = environment;
+    let url = `${appUrl}/posts`;
+
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+
+    return this.http.get<Post[]>(url);
+  }
+}
---
+++ Angular/10. Workshop - Forms/Project-workshop-forms/src/app/themes-list/themes-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+import { UserService } from '../user/user.service';
+
+@Component({
+  selector: 'app-themes-list',
+  templateUrl: './themes-list.component.html',
+  styleUrls: ['./themes-list.component.css'],
+})
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] | null = [];
+  isLoading: boolean = true;
+
+  constructor(private api: ApiService, private userService: UserService) {}
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    // this.api.getThemes().subscribe((themes) => {
+    //   console.log(themes);
+    //   this.themes = themes;
+    //   this.isLoading = false;
+    // });
+
+    this.api.getThemes().subscribe({
+      next: (themes) => {
+        console.log(themes);
+        this.themes = themes;
+        this.isLoading = false;
+      },
+
+      error: (err) => {
+        this.isLoading = false;
+        console.error('Error: ', err);
+      },
+    });
+  }
+}
