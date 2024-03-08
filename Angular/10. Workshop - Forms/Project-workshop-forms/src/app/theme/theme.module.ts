eb163598eae9acde13e932fa02b103fff07f7c8c
al-badarin <jamalalbadarin@gmail.com>
2024-03-08 21:46:59 +0200

    finished Workshop 10. Forms
    implemented:
    - 'Login' page - form's validations;
    - 'Register' page - form's validations;
    - 'New Theme' page - form's validations;
    - 'Profile' page - form's validations & 'Edit' functionality;

--- Angular/10. Workshop - Forms/Project-workshop-forms/src/app/theme/theme.module.ts
+++ Angular/10. Workshop - Forms/Project-workshop-forms/src/app/theme/theme.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,7 @@ import { ThemeRoutingModule } from './theme-routing.module';
     CurrentThemeComponent,
   ],
   imports: [
-    CommonModule, ThemeRoutingModule
+    CommonModule, ThemeRoutingModule, FormsModule
   ]
 })
 export class ThemeModule { }
