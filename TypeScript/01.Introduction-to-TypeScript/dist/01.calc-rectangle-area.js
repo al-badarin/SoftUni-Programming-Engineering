e6794858b1039a5733743dfe17e6ac0389f80410
al-badarin <jamalalbadarin@gmail.com>
2024-04-08 00:10:15 +0300

    solved task 1. Calculate Rectangle Area

---
+++ TypeScript/01.Introduction-to-TypeScript/dist/01.calc-rectangle-area.js
@@ -0,0 +1,6 @@
+const rectangleLength = 0;
+const rectangleWidth = 0;
+function calcArea(rectangleLength, rectangleWidth) {
+    return rectangleLength * rectangleWidth;
+}
+console.log(calcArea(5, 7));
---
+++ TypeScript/01.Introduction-to-TypeScript/src/01.calc-rectangle-area.ts
@@ -0,0 +1,8 @@
+const rectangleLength: number = 0;
+const rectangleWidth: number = 0;
+
+function calcArea(rectangleLength: number, rectangleWidth: number): number {
+  return rectangleLength * rectangleWidth;
+}
+
+console.log(calcArea(5, 7));
