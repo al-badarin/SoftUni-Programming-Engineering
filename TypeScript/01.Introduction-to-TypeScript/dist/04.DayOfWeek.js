71196635431fe07ec029f100907ce8fa56eabac4
al-badarin <jamalalbadarin@gmail.com>
2024-04-08 00:45:33 +0300

    solved task 04.DayOfWeek

---
+++ TypeScript/01.Introduction-to-TypeScript/dist/04.DayOfWeek.js
@@ -0,0 +1,34 @@
+function dayOfWeek(day) {
+    let dayNumber;
+    switch (day.toLowerCase()) {
+        case "monday":
+            dayNumber = 1;
+            break;
+        case "tuesday":
+            dayNumber = 2;
+            break;
+        case "wednesday":
+            dayNumber = 3;
+            break;
+        case "thursday":
+            dayNumber = 4;
+            break;
+        case "friday":
+            dayNumber = 5;
+            break;
+        case "saturday":
+            dayNumber = 6;
+            break;
+        case "sunday":
+            dayNumber = 7;
+            break;
+        default:
+            console.log("Error: Invalid day of the week.");
+            return;
+    }
+    console.log(`The number corresponding to ${day} is: ${dayNumber}`);
+}
+// Test the function
+dayOfWeek('Monday');
+dayOfWeek('Friday');
+dayOfWeek('Invalid');
