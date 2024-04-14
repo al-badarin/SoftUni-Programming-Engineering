13518048f8d238cd9c8fe81c431569984a2347c5
al-badarin <jamalalbadarin@gmail.com>
2024-04-14 18:51:14 +0300

    solved task '02.genericCompareElements'

---
+++ TypeScript/04.Generics/dist/02.genericCompareElements.js
@@ -0,0 +1,20 @@
+class CompareElements {
+    elements;
+    constructor(elements) {
+        this.elements = elements;
+    }
+    compare(comparator) {
+        let count = 0;
+        for (const element of this.elements) {
+            if (element === comparator) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+// Test the CompareElements class with different types of arrays
+let c1 = new CompareElements(["a", "b", "ab", "abc", "cba", "b"]);
+console.log(c1.compare("b")); // Output: 2
+let c2 = new CompareElements([1, 2, 3, 4, 5, 1, 1]);
+console.log(c2.compare(1)); // Output: 3
