85ebc4e1643b146731c2fef20f42807e4d7355f3
al-badarin <jamalalbadarin@gmail.com>
2024-04-24 00:56:16 +0300

    Update httpService.ts

---
+++ TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/services/httpService.ts
@@ -0,0 +1,51 @@
+import { fetchUtil } from "../utils/http";
+
+export class HttpService<T> {
+  protected apiUrl: string;
+  protected data = {} as T;
+  protected dataCollection = [] as T[];
+
+  constructor(apiUrl: string) {
+    this.apiUrl = apiUrl;
+  }
+
+  // CREATE
+  create(body: T) {
+    const config = {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+    return fetchUtil<T>(this.apiUrl, config);
+  }
+
+  // READ ALL
+  getAll() {
+    return fetchUtil<T[]>(this.apiUrl) as Promise<T[]>;
+  }
+
+  // READ ONE
+  getOne(id: number) {
+    return fetchUtil<T>(`${this.apiUrl}/${id}`) as Promise<T>;
+  }
+
+  // UPDATE
+  update<T extends { id: number }>(body: T) {
+    const config = {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+    return fetchUtil<T>(`${this.apiUrl}/${body.id}`, config);
+  }
+
+  // DELETE
+  delete(id: number) {
+    const config = { method: "DELETE" };
+    return fetchUtil<T>(`${this.apiUrl}/${id}`, config);
+  }
+}
