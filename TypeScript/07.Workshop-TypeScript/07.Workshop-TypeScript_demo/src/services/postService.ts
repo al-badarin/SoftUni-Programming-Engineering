9d635a073326092fef661c61d6443b23392c246c
al-badarin <jamalalbadarin@gmail.com>
2024-04-24 00:56:13 +0300

    Update postService.ts

--- TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/services/postService.ts
+++ TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/services/postService.ts
@@ -1,3 +1,15 @@
-export class PostService {
-    constructor(baseUrl: string) {}
+import { Post } from "../types/post";
+import { fetchUtil } from "../utils/http";
+import { HttpService } from "./httpService";
+
+export class PostService extends HttpService<Post> {
+  constructor(baseUrl: string) {
+    super(`${baseUrl}/posts`);
   }
+
+  getUserPosts(userId: number) {
+    return fetchUtil<Post[]>(`${this.apiUrl}?userId=${userId}`) as Promise<
+      Post[]
+    >;
+  }
+}
