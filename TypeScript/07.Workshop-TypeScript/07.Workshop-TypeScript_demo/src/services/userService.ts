59b4c97434e075963c44cb39eed5b82bb8e5e442
al-badarin <jamalalbadarin@gmail.com>
2024-04-24 00:56:09 +0300

    Update userService.ts

--- TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/services/userService.ts
+++ TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/services/userService.ts
@@ -1,3 +1,14 @@
-export class UserService {
-  constructor(baseUrl: string) {}
+import { UserDetails } from "../types/user";
+import { HttpService } from "./httpService";
+
+export class UserService extends HttpService<UserDetails> {
+  constructor(baseUrl: string) {
+    super(`${baseUrl}/users`);
+  }
+
+  getSingleUser(id: number, cb: Function) {
+    return this.getOne(id).then((data) => {
+      cb(data.id);
+    });
+  }
 }
