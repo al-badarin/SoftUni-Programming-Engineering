7192739d762bbd3435edf3faee3f550bdc8e1be2
al-badarin <jamalalbadarin@gmail.com>
2024-04-25 01:07:37 +0300

    updated solution 1 / added solution 2 to task 1.Calculator

--- TypeScript/08.Exam-Prep/1.Calculator/src/index.ts
+++ TypeScript/08.Exam-Prep/1.Calculator/src/index.ts
@@ -1,32 +1,58 @@
-function calculator(num1: number, operator: string, num2: number): void {
-  let res: number = 0;
+function calculator(num1: number, operator: string, num2: number): string {
+  let res = null;
+
   switch (operator) {
     case "+":
-      res = num1 + num2;
+      res = (num1 + num2).toFixed(2);
       break;
 
     case "-":
-      res = num1 - num2;
+      res = (num1 - num2).toFixed(2);
       break;
 
     case "*":
-      res = num1 * num2;
+      res = (num1 * num2).toFixed(2);
       break;
 
     case "/":
-      res = num1 / num2;
+      res = (num1 / num2).toFixed(2);
       break;
 
     default:
-      console.log("Invalid operator");
-      return;
+      res = "Non existing operator!";
+      break;
   }
 
-  console.log(res.toFixed(2));
+  return res;
 }
 
+// ** Solution 2
+// type DictCalc = {
+//   [key: string]: string;
+// };
+
+// function calculator(first: number, operator: string, second: number): string {
+//   const dictCalculator = {
+//     "+": (first + second).toFixed(2),
+//     "-": (first - second).toFixed(2),
+//     "/": (first / second).toFixed(2),
+//     "*": (first * second).toFixed(2),
+//   };
+
+//   if (!dictCalculator[operator]) {
+//     return "Non existing operator!";
+//   }
+
+//   return dictCalculator[operator];
+// }
+
 // Test cases
-calculator(5, "+", 10); // Output: 15.00
-calculator(25.5, "-", 3); // Output: 22.50
-calculator(9, "/", 2); // Output: 4.50
-calculator(7, "*", 5); // Output: 35.00
+const result1 = calculator(5, "+", 10); // Output: 15.00
+const result2 = calculator(25.5, "-", 3); // Output: 22.50
+const result3 = calculator(9, "/", 2); // Output: 4.50
+const result4 = calculator(7, "*", 5); // Output: 35.00
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
