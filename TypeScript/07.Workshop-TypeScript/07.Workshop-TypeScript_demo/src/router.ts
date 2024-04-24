605607ea69e92f2e7d1fca65ecc812444f1b586e
al-badarin <jamalalbadarin@gmail.com>
2024-04-25 00:23:20 +0300

    Update router.ts

--- TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/router.ts
+++ TypeScript/07.Workshop-TypeScript/07.Workshop-TypeScript_demo/src/router.ts
@@ -1,8 +1,10 @@
 import { RouterMap } from "./types/router";
+import { homePage } from "./views/home";
 import { postPage } from "./views/post";
 import { userPage } from "./views/user";
 
 export const router: RouterMap = {
-  "/": userPage,
+  "/": homePage,
+  "/user": userPage,
   "/post": postPage,
 };
