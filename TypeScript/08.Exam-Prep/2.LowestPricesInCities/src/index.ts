72952c85722e92d5ed26ce47564cfd5f89264b7a
al-badarin <jamalalbadarin@gmail.com>
2024-04-25 15:16:50 +0300

    solved task '2.LowestPricesInCities'

---
+++ TypeScript/08.Exam-Prep/2.LowestPricesInCities/src/index.ts
@@ -0,0 +1,38 @@
+interface ProductInfo {
+  price: number;
+  town: string;
+}
+
+function lowestPrices(input: string[]) {
+  const products: { [productName: string]: ProductInfo } = {};
+
+  // Parse input
+  input.forEach((entry) => {
+    const [town, product, priceStr] = entry.split(" | ");
+    const price = Number(priceStr);
+
+    if (!products[product] || price < products[product].price) {
+      products[product] = { price: price, town };
+    }
+  });
+
+  // Output lowest prices
+  for (const product in products) {
+    console.log(
+      `${product} -> ${products[product].price} (${products[product].town})`
+    );
+  }
+}
+
+// Test case
+const input = [
+  "Sample Town | Sample Product | 1000",
+  "Sample Town | Orange | 2",
+  "Sample Town | Peach | 1",
+  "Sofia | Orange | 3",
+  "Sofia | Peach | 2",
+  "New York | Sample Product | 1000.1",
+  "New York | Burger | 10",
+];
+
+lowestPrices(input);
