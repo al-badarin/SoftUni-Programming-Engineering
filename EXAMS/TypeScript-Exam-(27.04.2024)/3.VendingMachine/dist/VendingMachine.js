68e788f7f5029bfacb8f46830536448c941adc0d
al-badarin <jamalalbadarin@gmail.com>
2024-04-27 10:44:09 +0300

    task 3. solved

---
+++ EXAMS/TypeScript-Exam-(27.04.2024)/3.VendingMachine/dist/VendingMachine.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VendingMachine = void 0;
+class VendingMachine {
+    buttonCapacity;
+    drinks;
+    constructor(buttonCapacity) {
+        this.buttonCapacity = buttonCapacity;
+        this.drinks = [];
+    }
+    addDrink(drink) {
+        if (this.buttonCapacity > this.drinks.length) {
+            this.drinks.push(drink);
+        }
+    }
+    removeDrink(name) {
+        const searchedDrinkIndex = this.drinks.findIndex((drink) => drink.name === name);
+        if (searchedDrinkIndex !== -1) {
+            this.drinks.splice(searchedDrinkIndex, 1);
+            return true;
+        }
+        return false;
+    }
+    getLongest() {
+        const longestDrink = this.drinks.reduce((prev, current) => prev.volume > current.volume ? prev : current);
+        return longestDrink.toString();
+    }
+    getCheapest() {
+        const cheapestDrink = this.drinks.reduce((prev, current) => prev.price < current.price ? prev : current);
+        return cheapestDrink.toString();
+    }
+    buyDrink(name) {
+        const drinkToBuy = this.drinks.find((drink) => drink.name === name);
+        if (drinkToBuy) {
+            // !this logic is not applied because it changes the expected output from the task description
+            //  this.removeDrink(name);
+            return drinkToBuy.toString();
+        }
+        else {
+            return `Error: The drink '${name}' is not found in the vending machine.`;
+        }
+    }
+    getCount() {
+        let availableDrinks = this.drinks.length;
+        return availableDrinks;
+    }
+    report() {
+        const drinksInfo = this.drinks.map((drink) => drink.toString()).join("\n");
+        return `Drinks available:\n${drinksInfo}`;
+    }
+}
+exports.VendingMachine = VendingMachine;
