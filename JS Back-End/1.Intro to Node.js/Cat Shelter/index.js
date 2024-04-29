f0b7b56887098bfafc7df30e2a1836ab95141cea
al-badarin <jamalalbadarin@gmail.com>
2024-04-30 00:58:51 +0300

    Update index.js

--- JS Back-End/1.Intro to Node.js/Cat Shelter/index.js
+++ JS Back-End/1.Intro to Node.js/Cat Shelter/index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const { parse } = require("querystring");
+
 const homeTemplate = require("./views/home.html");
 const addCatTemplate = require("./views/addCat.html");
 const addBreedTemplate = require("./views/addBreed.html");
@@ -38,36 +40,54 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "content-type": "text/html",
     });
-
     res.write(homeTemplate(cats));
     res.end();
   } else if (req.url === "/styles/site.css") {
     res.writeHead(200, {
       "content-type": "text/css",
     });
-
     res.write(siteCss);
     res.end();
+  } else if (req.url === "/cats/add-cat" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // Convert Buffer to string
+    });
+    req.on("end", () => {
+      const formData = parse(body);
+      console.log("Form Data:", formData); // Check form data
+      const newCat = {
+        id: cats.length + 1,
+        name: formData.name,
+        imageUrl: formData.image,
+        breed: formData.breed,
+        description: formData.description,
+      };
+      console.log("New Cat:", newCat); // Check new cat object
+      cats.push(newCat);
+      res.writeHead(302, { Location: "/" }); // Redirect to home page after adding cat
+      res.end();
+    });
   } else if (req.url === "/cats/add-cat") {
+    // Handle GET request to /cats/add-cat
     res.writeHead(200, {
       "content-type": "text/html",
     });
-
     res.write(addCatTemplate);
     res.end();
   } else if (req.url === "/cats/add-breed") {
+    // Handle request to /cats/add-breed
     res.writeHead(200, {
       "content-type": "text/html",
     });
-
     res.write(addBreedTemplate);
     res.end();
   } else {
-    res.writeHead(200, {
+    // Handle other requests (404)
+    res.writeHead(404, {
       "content-type": "text/html",
     });
-
-    res.write("<h1>404</h1>");
+    res.write("<h1>404 Not Found</h1>");
     res.end();
   }
 });
