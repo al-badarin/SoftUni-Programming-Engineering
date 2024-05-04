dc434c06784624587fc5296abb906115dd6ebec1
al-badarin <jamalalbadarin@gmail.com>
2024-05-05 00:38:50 +0300

    Update logger.js

--- JS Back-End/2.Node.js Streams and Utilities/pubsub/logger.js
+++ JS Back-End/2.Node.js Streams and Utilities/pubsub/logger.js
@@ -1,5 +1,9 @@
+const messageBroker = require("./messageBroker");
+
+messageBroker.subscribe("request", log);
+
 function log(message) {
-  console.log(message);
+  console.log("Logger: " + message);
 }
 
 module.exports = log;
