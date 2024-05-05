5df192ccf439920f66e48ca8baf96b5e986d838a
al-badarin <jamalalbadarin@gmail.com>
2024-05-05 19:56:10 +0300

    Update readFile.js
    ASYNCHRONOUS with Promises

--- JS Back-End/2.Node.js Streams and Utilities/fs/readFile.js
+++ JS Back-End/2.Node.js Streams and Utilities/fs/readFile.js
@@ -1,20 +1,28 @@
-const fs = require("fs");
+// const fs = require("fs");
+const fs = require("fs/promises");
 
-// SYNCHRONOUS
+// *SYNCHRONOUS
 // const text = fs.readFileSync("./data.txt", { encoding: "utf-8" });
 // console.log(text);
 
-// ASYNCHRONOUS with Callback
-console.log(1);
-const text = fs.readFile("./data.txt", { encoding: "utf-8" }, (err, result) => {
-  if (err) {
+// **ASYNCHRONOUS with Callback
+// console.log(1);
+// const text = fs.readFile("./data.txt", { encoding: "utf-8" }, (err, result) => {
+//   if (err) {
+//     console.log("There is a problem with the filesystem");
+//     return;
+//   }
+//   console.log(2);
+
+//   console.log(text);
+// });
+// console.log(3);
+
+// ***ASYNCHRONOUS with Promises
+fs.readFile("./data.txt", { encoding: "utf-8" })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
     console.log("There is a problem with the filesystem");
-    return;
-  }
-  console.log(2);
-
-  console.log(text);
-});
-console.log(3);
-
-// ASYNCHRONOUS with Promises
+  });
