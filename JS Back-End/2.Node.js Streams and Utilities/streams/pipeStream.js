20a2eadb9bb6e498538117d12c2f19764245ca83
al-badarin <jamalalbadarin@gmail.com>
2024-05-05 19:33:41 +0300

    Create pipeStream.js

---
+++ JS Back-End/2.Node.js Streams and Utilities/streams/pipeStream.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./data/input.txt", {
+  encoding: "utf-8",
+  highWaterMark: 1000,
+});
+
+const writeStream = fs.createWriteStream("./data/copy-pipeStream.txt", "utf-8");
+
+readStream.pipe(writeStream);
