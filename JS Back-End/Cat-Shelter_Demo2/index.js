0ec10a94a2f6a584a74b98c6021e429e061813eb
al-badarin <jamalalbadarin@gmail.com>
2024-05-06 13:15:00 +0300

    Create index.js

---
+++ JS Back-End/Cat-Shelter_Demo2/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 3000;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+
+    res.write("Hello Cat Shelter!");
+    res.end();
+  })
+  .listen(port);
+
+console.log(`Server is listening on port ${port}...`);
