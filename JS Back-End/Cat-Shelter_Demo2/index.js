9bb8488d89e1427d85ecb8fb271d5bf69ad1dea0
al-badarin <jamalalbadarin@gmail.com>
2024-05-07 00:35:42 +0300

    Update index.js

--- JS Back-End/Cat-Shelter_Demo2/index.js
+++ JS Back-End/Cat-Shelter_Demo2/index.js
@@ -1,15 +1,45 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const port = 3000;
+const handlers = require("./handlers");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, {
-      "Content-Type": "text/plain",
-    });
-
-    res.write("Hello Cat Shelter!");
-    res.end();
+    for (let handler of handlers) {
+      if (!handler(req, res)) {
+        break;
+      }
+    }
   })
-  .listen(port);
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+  });
 
-console.log(`Server is listening on port ${port}...`);
+// http
+//   .createServer((req, res) => {
+//     console.log("Requested Path:", req.url);
+
+//     if (req.url === "/") {
+//       // Serve the home page HTML
+//       const filePath = path.join(__dirname, "index.html"); // Make sure the path is correct
+//       fs.readFile(filePath, (err, data) => {
+//         if (err) {
+//           res.writeHead(404);
+//           res.end("Error: File Not Found");
+//         } else {
+//           res.writeHead(200, { "Content-Type": "text/html" });
+//           res.end(data);
+//         }
+//       });
+//     } else if (req.url === "/favicon.ico") {
+//       res.writeHead(204);
+//       res.end();
+//     } else {
+//       res.writeHead(404);
+//       res.end("Not Found");
+//     }
+//   })
+//   .listen(port, () => {
+//     console.log(`Server is listening on port ${port}...`);
+//   });
