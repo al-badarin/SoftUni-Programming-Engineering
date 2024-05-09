7f838ff0e4b2764ee4e021c682f45da8ab494f30
al-badarin <jamalalbadarin@gmail.com>
2024-05-09 19:47:20 +0300

    Generating a dynamic HTML for each cat

--- JS Back-End/Cat-Shelter_Demo2/handlers/home.js
+++ JS Back-End/Cat-Shelter_Demo2/handlers/home.js
@@ -1,7 +1,7 @@
 const url = require("url");
 const fs = require("fs");
 const path = require("path");
-// const cats = require("../data/cats");
+const cats = require("../data/cats");
 // const breeds = require("../data/breeds");
 
 module.exports = (req, res) => {
@@ -12,20 +12,35 @@ module.exports = (req, res) => {
       path.join(__dirname, "../views/home/index.html")
     );
 
-    fs.readFile(filePath, (err, data) => {
+    fs.readFile(filePath, "utf-8", (err, data) => {
       if (err) {
-        console.log(err);
-
+        console.error(err);
         res.writeHead(404, { "Content-Type": "text/plain" });
-
         res.write("Error: File Not Found");
         res.end();
         return;
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
       }
+      // Generate HTML for each cat
+      let modifiedCats = cats
+        .map((cat) => {
+          return `<li>
+        <img src="${cat.image}" alt="${cat.name}">
+        <h3>${cat.name}</h3>
+        <p><span>Breed: </span>${cat.breed}</p>
+        <p><span>Description: </span>${cat.description}</p>
+        <ul class="buttons">
+          <li class="btn edit"><a href="/cats-edit/${cat.id}">Change Info</a></li>
+          <li class="btn delete"><a href="/cats-find-new-home/${cat.id}">New Home</a></li>
+        </ul>
+      </li>`;
+        })
+        .join("");
+
+      let modifiedData = data.replace("{{cats}}", modifiedCats);
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(modifiedData);
+      res.end();
     });
   } else {
     return true;
