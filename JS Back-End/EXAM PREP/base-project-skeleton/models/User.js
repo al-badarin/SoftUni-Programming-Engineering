a560ddc8bedf451a10c6f569a5fc84383161d418
al-badarin <jamalalbadarin@gmail.com>
2024-06-12 04:03:41 +0300

    Add user model

---
+++ JS Back-End/EXAM PREP/base-project-skeleton/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    //TODO: validations can be different!
+    lowercase: true,
+    unique: true,
+    match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, "Invalid Email Address"],
+    minLength: [10, "Email should be at least 10 characters"],
+  },
+  password: {
+    type: String,
+    required: true,
+    //TODO: validations can be different!
+    minLength: [6, "Password should be at least 6 characters"],
+    match: [/^[a-zA-Z0-9]+$/, "Password should be alphanumeric"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
