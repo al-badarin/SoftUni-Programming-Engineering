5654006e12dd5b79db678a972c6e468958767279
al-badarin <jamalalbadarin@gmail.com>
2024-06-13 02:45:24 +0300

    Add authService login method      - validate user

--- JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
+++ JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
@@ -1,3 +1,17 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.register = (userData) => User.create(userData);
+
+exports.register = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Email or passowrd is invalid");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Email or passowrd is invalid");
+  }
+};
