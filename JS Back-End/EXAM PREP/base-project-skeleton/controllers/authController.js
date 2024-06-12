8009ee656c45568e6fc34200d62e8288e22c909e
al-badarin <jamalalbadarin@gmail.com>
2024-06-13 01:33:56 +0300

    Add post register action

--- JS Back-End/EXAM PREP/base-project-skeleton/controllers/authController.js
+++ JS Back-End/EXAM PREP/base-project-skeleton/controllers/authController.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
+const authService = require("../services/authServices");
+
 router.get("/register", (req, res) => {
   res.render("auth/register");
+});
+
+router.post("/register", async (req, res) => {
+  const userData = req.body;
+
+  await authService.register(userData);
 });
 
 module.exports = router;
