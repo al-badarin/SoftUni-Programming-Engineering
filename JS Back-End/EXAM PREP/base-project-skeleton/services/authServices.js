6612c908074164960c63e4aa4c4801a68b2dc187
al-badarin <jamalalbadarin@gmail.com>
2024-06-15 16:54:35 +0300

    Update authServices.js

--- JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
+++ JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("../lib/jsonwebtoken");
 
-const SECRET = "jfew8u4i3qhng09384gurh9308hfuwqgn";
+const { SECRET } = require("../config");
 
 exports.register = async (userData) => {
   if (userData.password !== userData.rePassword) {
