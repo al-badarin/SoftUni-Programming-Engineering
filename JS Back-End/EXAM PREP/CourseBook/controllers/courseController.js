67f7f3bd27876f0b9effb04b69889fb2ef1964cf
al-badarin <jamalalbadarin@gmail.com>
2024-06-16 19:13:30 +0300

    add creating course functionality

--- JS Back-End/EXAM PREP/CourseBook/controllers/courseController.js
+++ JS Back-End/EXAM PREP/CourseBook/controllers/courseController.js
@@ -1,7 +1,26 @@
+const { isAuth } = require("../middlewares/authMiddleware");
+const { getErrorMessage } = require("../utils/errorUtils");
+
+const courseService = require("../services/courseService");
 const router = require("express").Router();
 
-router.get("/create", (req, res) => {
+router.get("/create", isAuth, (req, res) => {
   res.render("courses/create");
+});
+
+router.post("/create", isAuth, async (req, res) => {
+  const courseData = req.body;
+
+  try {
+    await courseService.create(req.user._id, courseData);
+
+    res.redirect("/courses");
+  } catch (err) {
+    res.render("courses/create", {
+      ...courseData,
+      error: getErrorMessage(err),
+    });
+  }
 });
 
 module.exports = router;
