d7bbd5a68ef6e06ffb8d6ea3a828803e6e26a530
al-badarin <jamalalbadarin@gmail.com>
2024-06-17 15:15:41 +0300

    render static details page

--- JS Back-End/EXAM PREP/CourseBook/services/courseService.js
+++ JS Back-End/EXAM PREP/CourseBook/services/courseService.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 
 exports.getAll = () => Course.find();
 
+exports.getOne = (courseId) => Course.findById(courseId);
+
 exports.create = async (userId, courseData) => {
   const createdCourse = await Course.create({
     owner: userId,
