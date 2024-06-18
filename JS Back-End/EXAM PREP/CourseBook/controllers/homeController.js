14f4d18df6fa68d0b73bd4b6eb1c4421d0bec310
al-badarin <jamalalbadarin@gmail.com>
2024-06-19 00:17:45 +0300

    add profile page

--- JS Back-End/EXAM PREP/CourseBook/controllers/homeController.js
+++ JS Back-End/EXAM PREP/CourseBook/controllers/homeController.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 const courseService = require("../services/courseService");
+const userService = require("../services/userService");
+
+const { isAuth } = require("../middlewares/authMiddleware");
 
 router.get("/", async (req, res) => {
   const latestCourses = await courseService.getLatest().lean();
 
   res.render("home", { latestCourses });
+});
+
+router.get("/profile", isAuth, async (req, res) => {
+  const user = await userService.getInfo(req.user._id).lean();
+
+  const createdCoursesCount = user.createdCourses?.length || 0;
+  const signedUpCoursesCount = user.signedUpCourses?.length || 0;
+
+  res.render("profile", { user, createdCoursesCount, signedUpCoursesCount });
 });
 
 module.exports = router;
