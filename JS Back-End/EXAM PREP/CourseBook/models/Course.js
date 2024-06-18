bdd3aec28b3959eb38ecf3d2960e75ba51eea1e2
al-badarin <jamalalbadarin@gmail.com>
2024-06-19 00:49:08 +0300

    add timestamps

--- JS Back-End/EXAM PREP/CourseBook/models/Course.js
+++ JS Back-End/EXAM PREP/CourseBook/models/Course.js
@@ -1,60 +1,63 @@
 const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-    minlength: [5, "Title should be at least 5 characters"],
-  },
-  type: {
-    type: String,
-    required: [true, "Type is required"],
-    minlength: [3, "Type should be a minimum of 3 characters long"],
-  },
-  certificate: {
-    type: String,
-    required: [true, "Certificate is required"],
-    minlength: [2, "Certificate should be a minimum of 2 characters long"],
-  },
-  image: {
-    type: String,
-    required: [true, "Image is required"],
-    match: [
-      /^https?:\/\//,
-      "Course Image should start with http:// or https://",
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      minlength: [5, "Title should be at least 5 characters"],
+    },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+      minlength: [3, "Type should be a minimum of 3 characters long"],
+    },
+    certificate: {
+      type: String,
+      required: [true, "Certificate is required"],
+      minlength: [2, "Certificate should be a minimum of 2 characters long"],
+    },
+    image: {
+      type: String,
+      required: [true, "Image is required"],
+      match: [
+        /^https?:\/\//,
+        "Course Image should start with http:// or https://",
+      ],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      minlength: [10, "Description should be a minimum of 10 characters long"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price must be a positive number"],
+    },
+    signUpList: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
     ],
-  },
-  description: {
-    type: String,
-    required: [true, "Description is required"],
-    minlength: [10, "Description should be a minimum of 10 characters long"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Price is required"],
-    min: [0, "Price must be a positive number"],
-  },
-  signUpList: [
-    {
+    // createdAt: {
+    //   type: Date,
+    // },
+    owner: {
       type: mongoose.Types.ObjectId,
       ref: "User",
+      required: [true, "Owner is required"],
     },
-  ],
-  createdAt: {
-    type: Date,
   },
-  owner: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Owner is required"],
-  },
-});
+  { timestamps: true }
+);
 
-courseSchema.pre("save", function () {
-  if (!this.createdAt) {
-    this.createdAt = Date.now();
-  }
-});
+// courseSchema.pre("save", function () {
+//   if (!this.createdAt) {
+//     this.createdAt = Date.now();
+//   }
+// });
 
 const Course = mongoose.model("Course", courseSchema);
 
--- JS Back-End/EXAM PREP/CourseBook/models/User.js
+++ JS Back-End/EXAM PREP/CourseBook/models/User.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    minLength: [2, "The username should be at least 2 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    lowercase: true,
-    unique: true,
-    match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, "Invalid Email Address"],
-    minLength: [10, "Email should be at least 10 characters"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minLength: [4, "Password should be at least 4 characters"],
-  },
-  createdCourses: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Course",
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      minLength: [2, "The username should be at least 2 characters"],
     },
-  ],
-  signedUpCourses: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Course",
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      unique: true,
+      match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, "Invalid Email Address"],
+      minLength: [10, "Email should be at least 10 characters"],
     },
-  ],
-});
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [4, "Password should be at least 4 characters"],
+    },
+    createdCourses: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    signedUpCourses: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 12);
