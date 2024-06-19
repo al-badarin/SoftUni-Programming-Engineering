fe9ae6aa1060a691f6ad4cdfd5a42be6eb1d5c1b
al-badarin <jamalalbadarin@gmail.com>
2024-06-19 22:39:13 +0300

    render stone details page with dynamic data

--- JS Back-End/EXAM PREP/EarthTreasureVaul/EarthTreasureVaul/controllers/stoneController.js
+++ JS Back-End/EXAM PREP/EarthTreasureVaul/EarthTreasureVaul/controllers/stoneController.js
@@ -10,6 +10,14 @@ router.get("/", async (req, res) => {
   res.render("stones/dashboard", { stones });
 });
 
+router.get("/:stoneId/details", async (req, res) => {
+  const stone = await stoneServices.getOne(req.params.stoneId).lean();
+
+  const isOwner = stone.owner && stone.owner._id == req.user?._id;
+
+  res.render("stones/details", { ...stone, isOwner });
+});
+
 router.get("/create", isAuth, (req, res) => {
   res.render("stones/create");
 });
--- JS Back-End/EXAM PREP/EarthTreasureVaul/EarthTreasureVaul/services/stoneServices.js
+++ JS Back-End/EXAM PREP/EarthTreasureVaul/EarthTreasureVaul/services/stoneServices.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 
 exports.getAll = () => Stone.find();
 
+exports.getOne = (stoneId) => Stone.findById(stoneId).populate("owner")
+
 exports.create = async (userId, stoneData) => {
   const createdStone = await Stone.create({
     owner: userId,
