f89cbad6278343d54f2e67b62613a274f4074e76
al-badarin <jamalalbadarin@gmail.com>
2024-06-21 19:24:40 +0300

    Update Volcano.js

--- JS Back-End/EXAM PREP/MagmaHaven/models/Volcano.js
+++ JS Back-End/EXAM PREP/MagmaHaven/models/Volcano.js
@@ -4,28 +4,28 @@ const volcanoSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "Name is required"],
       minlength: [2, "Name should be at least 2 characters long"],
     },
     location: {
       type: String,
-      required: true,
+      required: [true, "Location is required"],
       minlength: [3, "Location should be at least 3 characters long"],
     },
     elevation: {
       type: Number,
-      required: true,
+      required: [true, "Elevation is required"],
       min: [0, "Elevation should be a minimum of 0"],
     },
     lastEruption: {
       type: Number,
-      required: true,
+      required: [true, "Year of Last Eruption is required"],
       min: [0, "Year of Last Eruption should be between 0 and 2024"],
       max: [2024, "Year of Last Eruption should be between 0 and 2024"],
     },
     image: {
       type: String,
-      required: true,
+      required: [true, "Image URL is required"],
       match: [
         /^https?:\/\/.*/,
         "Volcano Image should start with http:// or https://",
@@ -33,7 +33,7 @@ const volcanoSchema = new mongoose.Schema(
     },
     typeVolcano: {
       type: String,
-      required: true,
+      required: [true, "Type of Volcano is required"],
       enum: [
         "Supervolcanoes",
         "Submarine",
@@ -45,7 +45,7 @@ const volcanoSchema = new mongoose.Schema(
     },
     description: {
       type: String,
-      required: true,
+      required: [true, "Description is required"],
       minlength: [10, "Description should be a minimum of 10 characters long"],
     },
     voteList: [
@@ -57,7 +57,7 @@ const volcanoSchema = new mongoose.Schema(
     owner: {
       type: mongoose.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: [true, "Owner is required"],
     },
   },
   { timestamps: true }
