3a78f4e25d7fad48665140a4f32e29e04d08f5b8
al-badarin <jamalalbadarin@gmail.com>
2024-06-21 17:22:29 +0300

    add 404 page and routes

--- JS Back-End/EXAM PREP/MagmaHaven/routes.js
+++ JS Back-End/EXAM PREP/MagmaHaven/routes.js
@@ -4,6 +4,10 @@ const homeController = require("./controllers/homeController");
 const authController = require("./controllers/authController");
 
 router.use(homeController);
-router.use('/auth', authController);
+router.use("/auth", authController);
+
+router.all("*", (req, res) => {
+  res.render("404");
+});
 
 module.exports = router;
