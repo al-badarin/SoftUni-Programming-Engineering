65e82659a64b37b946f3f274577c877c264868b3
al-badarin <jamalalbadarin@gmail.com>
2024-06-22 00:13:54 +0300

    Update authServices.js

--- JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
+++ JS Back-End/EXAM PREP/base-project-skeleton/services/authServices.js
@@ -9,8 +9,8 @@ exports.register = async (userData) => {
     throw new Error("Password missmatch");
   }
 
-  const user = await User.findOne({ email: userData.email });
-  if (user) {
+  const existingUser = await User.findOne({ email: userData.email });
+  if (existingUser) {
     throw new Error("User already exists");
   }
 
@@ -22,16 +22,15 @@ exports.register = async (userData) => {
 };
 
 exports.login = async ({ email, password }) => {
-  // Check if user exists
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw new Error("Email or passowrd is invalid");
+    throw new Error("Email or password is invalid");
   }
 
   const isValid = await bcrypt.compare(password, user.password);
   if (!isValid) {
-    throw new Error("Email or passowrd is invalid");
+    throw new Error("Email or password is invalid");
   }
 
   const token = await generateToken(user);
