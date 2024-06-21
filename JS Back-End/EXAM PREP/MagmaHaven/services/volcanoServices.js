4fc0e7e61a33076b99e49edaa02023704a2bc88e
al-badarin <jamalalbadarin@gmail.com>
2024-06-21 18:55:24 +0300

    Create volcanoServices.js

---
+++ JS Back-End/EXAM PREP/MagmaHaven/services/volcanoServices.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const Volcano = require("../models/Volcano");
+
+exports.getAll = () => Volcano.find();
+
+exports.getOne = (volcanoId) => Volcano.findById(volcanoId);
+
+exports.getLatest = () => Volcano.find().sort({ createdAt: -1 }).limit(3);
+
+exports.vote = async (volcanoId, userId) => {
+  const volcano = await Volcano.findById(volcanoId);
+  const user = await User.findById(userId);
+
+  volcano.voteList.push(userId);
+  user.votedForVolcanoes.push(volcanoId);
+
+  await volcano.save();
+  await user.save();
+};
+
+exports.create = async (userId, volcanoData) => {
+  const createdVolcano = await Volcano.create({
+    owner: userId,
+    ...volcanoData,
+  });
+
+  await User.findByIdAndUpdate(userId, {
+    $push: { createdVolcanoes: createdVolcano._id },
+  });
+
+  return createdVolcano;
+};
+
+exports.edit = (volcanoId, volcanoData) =>
+  Volcano.findByIdAndUpdate(volcanoId, volcanoData, { runValidators: true });
+
+exports.delete = (volcanoId) => Volcano.findByIdAndDelete(volcanoId);
