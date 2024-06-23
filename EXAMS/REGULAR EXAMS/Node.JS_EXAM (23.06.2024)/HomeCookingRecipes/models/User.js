eddc7114d8658ec39b5ce9da526a8a56c709cd31
al-badarin <jamalalbadarin@gmail.com>
2024-06-23 14:28:26 +0300

    Update User.js

--- EXAMS/REGULAR EXAMS/Node.JS_EXAM (23.06.2024)/HomeCookingRecipes/models/User.js
+++ EXAMS/REGULAR EXAMS/Node.JS_EXAM (23.06.2024)/HomeCookingRecipes/models/User.js
@@ -22,6 +22,18 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minLength: [4, "Password should be at least 4 characters"],
     },
+    createdRecipes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Recipe",
+      },
+    ],
+    recommendedRecipes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Recipe",
+      },
+    ],
   },
   { timestamps: true }
 );
