8ed9ab946e77790046606f12471006492dfe4050
al-badarin <jamalalbadarin@gmail.com>
2024-06-23 14:20:48 +0300

    Update User.js

--- EXAMS/REGULAR EXAMS/Node.JS_EXAM (23.06.2024)/HomeCookingRecipes/models/User.js
+++ EXAMS/REGULAR EXAMS/Node.JS_EXAM (23.06.2024)/HomeCookingRecipes/models/User.js
@@ -6,22 +6,21 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: [true, "Username is required"],
+      minLength: [2, "Username should be at least 2 characters"],
+      maxLength: [20, "Username should be maximum 20 characters"],
     },
     email: {
       type: String,
       required: [true, "Email is required"],
-      //TODO: validations can be different!
       lowercase: true,
       unique: true,
-      match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, "Invalid Email Address"],
+      // match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, "Invalid Email Address"],
       minLength: [10, "Email should be at least 10 characters"],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      //TODO: validations can be different!
-      minLength: [6, "Password should be at least 6 characters"],
-      match: [/^[a-zA-Z0-9]+$/, "Password should be alphanumeric"],
+      minLength: [4, "Password should be at least 4 characters"],
     },
   },
   { timestamps: true }
@@ -29,7 +28,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function () {
   if (this.isModified("password")) {
-  this.password = await bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hash(this.password, 12);
   }
 });
 
