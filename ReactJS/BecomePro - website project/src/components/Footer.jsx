77b0e2c5b8f15e21abd40af5df0d93c0d5d4e70d
al-badarin <jamalalbadarin@gmail.com>
2024-06-27 02:18:07 +0300

    folder moved

--- ReactJS/BecomePro - website project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="container-fluid footer_section">
-      <p>
-         All Rights Reserved. &copy; BecomePro 2024
-      </p>
-    </footer>
-  );
-}
