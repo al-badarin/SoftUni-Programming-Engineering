0e664236a6fd30f35ad7146e776876e56b0e7b7f
al-badarin <jamalalbadarin@gmail.com>
2024-07-05 00:50:54 +0300

    add UserList component + header and search

--- ReactJS/Workshop_Components/User-List-Workshop/user-list/src/App.jsx
+++ ReactJS/Workshop_Components/User-List-Workshop/user-list/src/App.jsx
@@ -1,9 +1,15 @@
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./styles.css";
 
 function App() {
   return (
     <div>
+      <Header />
+
+      <main class="main">
+        
+      </main>
       <Footer />
     </div>
   );
