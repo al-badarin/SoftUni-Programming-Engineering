8205327ba1dad0828e0894af9c995c671db210e9
al-badarin <jamalalbadarin@gmail.com>
2024-07-13 21:01:26 +0300

    add GameDetails

--- ReactJS/Workshop_Routing/GamesPlay/client/src/App.jsx
+++ ReactJS/Workshop_Routing/GamesPlay/client/src/App.jsx
@@ -1,11 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Header from "./header/Header";
-import Home from "./home/Home";
-import GameList from "./game-list/GameList";
-import GameCreate from "./game-create/GameCreate";
-import Login from "./login/Login";
-import Register from "./register/Register";
+import Header from './header/Header';
+import Home from './home/Home';
+import GameList from './game-list/GameList';
+import GameCreate from './game-create/GameCreate';
+import Login from './login/Login';
+import Register from './register/Register';
+import GameDetails from './game-details/GameDetails';
 
 function App() {
   return (
@@ -17,10 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
+        <Route path="/games/:gameId" element={<GameDetails />} />
         <Route path="/games/create" element={<GameCreate />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="" element={<xxx />} />
       </Routes>
     </>
   );
