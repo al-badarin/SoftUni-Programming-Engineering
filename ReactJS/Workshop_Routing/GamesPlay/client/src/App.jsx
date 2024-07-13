6a5ef0cf7a479ea1e8b336c7603af253e05ef364
al-badarin <jamalalbadarin@gmail.com>
2024-07-13 18:44:45 +0300

    add GameCreate

--- ReactJS/Workshop_Routing/GamesPlay/client/src/App.jsx
+++ ReactJS/Workshop_Routing/GamesPlay/client/src/App.jsx
@@ -1,8 +1,9 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./header/Header";
-import Home from './home/Home';
-import GameList from './game-list/GameList';
+import Home from "./home/Home";
+import GameList from "./game-list/GameList";
+import GameCreate from "./game-create/GameCreate";
 
 function App() {
   return (
@@ -12,12 +13,12 @@ function App() {
       </div>
 
       <Routes>
-        <Route path='/' element={< Home/>}/>
-        <Route path='/games' element={< GameList/>}/>
-        <Route path='' element={< xxx/>}/>
-        <Route path='' element={< xxx/>}/>
-        <Route path='' element={< xxx/>}/>
-        <Route path='' element={< xxx/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+        <Route path="" element={<xxx />} />
+        <Route path="" element={<xxx />} />
+        <Route path="" element={<xxx />} />
       </Routes>
     </>
   );
