589806810a3b80617f6f6719194fd74637db936a
al-badarin <jamalalbadarin@gmail.com>
2024-07-16 12:55:37 +0300

    Add login form keys

--- ReactJS/Workshop_Routing/GamesPlay/client/src/login/Login.jsx
+++ ReactJS/Workshop_Routing/GamesPlay/client/src/login/Login.jsx
@@ -1,15 +1,19 @@
 import useForm from '../hooks/useForm';
 
-export default function Login() {
-  const { values, onChange, onSubmit } = useForm({
-    email: '',
-    password: '',
+const LoginFormKeyes = {
+  Email: 'email',
+  Password: 'password',
+};
+
+export default function Login({ loginSubmitHandler }) {
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+    [LoginFormKeyes.Email]: '',
+    [LoginFormKeyes.Password]: '',
   });
 
   return (
     <section id="login-page" className="auth">
       <form id="login" onSubmit={onSubmit}>
-        
         <div className="container">
           <div className="brand-logo"></div>
           <h1>Login</h1>
@@ -17,19 +21,19 @@ export default function Login() {
           <input
             type="email"
             id="email"
-            name="email"
+            name={LoginFormKeyes.Email}
             placeholder="Sokka@gmail.com"
             onChange={onChange}
-            value={values.email}
+            value={values[LoginFormKeyes.Email]}
           />
 
           <label htmlFor="login-pass">Password:</label>
           <input
             type="password"
             id="login-password"
-            name="password"
+            name={LoginFormKeyes.Password}
             onChange={onChange}
-            value={values.password}
+            value={values[LoginFormKeyes.Password]}
           />
           <input type="submit" className="btn submit" value="Login" />
           <p className="field">
