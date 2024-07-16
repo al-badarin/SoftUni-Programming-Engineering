72df4ee99c128100341d7feb6e58af16c43a9085
al-badarin <jamalalbadarin@gmail.com>
2024-07-16 12:45:12 +0300

    Change login to controlled form

--- ReactJS/Workshop_Routing/GamesPlay/client/src/login/Login.jsx
+++ ReactJS/Workshop_Routing/GamesPlay/client/src/login/Login.jsx
@@ -1,7 +1,15 @@
+import useForm from '../hooks/useForm';
+
 export default function Login() {
+  const { values, onChange, onSubmit } = useForm({
+    email: '',
+    password: '',
+  });
+
   return (
     <section id="login-page" className="auth">
-      <form id="login">
+      <form id="login" onSubmit={onSubmit}>
+        
         <div className="container">
           <div className="brand-logo"></div>
           <h1>Login</h1>
@@ -11,14 +19,22 @@ export default function Login() {
             id="email"
             name="email"
             placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values.email}
           />
 
           <label htmlFor="login-pass">Password:</label>
-          <input type="password" id="login-password" name="password" />
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
           <input type="submit" className="btn submit" value="Login" />
           <p className="field">
             <span>
-              If you don't have profile click <a href="#">here</a>
+              If you don't have profile click <a href="/register">here</a>
             </span>
           </p>
         </div>
